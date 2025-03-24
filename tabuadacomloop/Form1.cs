2b95a2126764f05c81f775a79a92be92a697d43b
koitialex <koitialex@gmail.com>
2025-03-24 17:13:04 -0300

    feat: tabuada terminada

---
+++ tabuadacomloop/Form1.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace tabuadacomloop
+{
+    public partial class frmTabuadaComLoop : Form
+    {
+        public frmTabuadaComLoop()
+        {
+            InitializeComponent();
+            this.AcceptButton = btnExecutar;
+        }
+
+        private void btnExecutar_Click(object sender, EventArgs e)
+        {
+            int numeroInserido = Convert.ToInt32(txtNumero.Text);
+            lstTabuada.Items.Clear();
+
+            for (int i = 1; i < 11; i++)
+            {
+                int resultado = numeroInserido * i;
+                lstTabuada.Items.Add($"{numeroInserido} x {i}  = {resultado}");
+            }
+
+
+        }
+    }
+}
