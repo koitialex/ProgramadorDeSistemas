1ff95b5d2a846a0b15a278793a35c44c1b5fb422
koitialex <koitialex@gmail.com>
2025-03-25 14:31:34 -0300

    feat:

---
+++ arrayUnimendicional/Form1.Designer.cs
@@ -0,0 +1,122 @@
+﻿
+namespace arrayUnimendicional
+{
+    partial class frmVetor
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.pnlTop = new System.Windows.Forms.Panel();
+            this.pnlBottom = new System.Windows.Forms.Panel();
+            this.lblTopo = new System.Windows.Forms.Label();
+            this.lblBottom = new System.Windows.Forms.Label();
+            this.btnTestar = new System.Windows.Forms.Button();
+            this.pnlTop.SuspendLayout();
+            this.pnlBottom.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // pnlTop
+            // 
+            this.pnlTop.BackColor = System.Drawing.Color.Navy;
+            this.pnlTop.Controls.Add(this.lblTopo);
+            this.pnlTop.Dock = System.Windows.Forms.DockStyle.Top;
+            this.pnlTop.Location = new System.Drawing.Point(0, 0);
+            this.pnlTop.Name = "pnlTop";
+            this.pnlTop.Size = new System.Drawing.Size(800, 100);
+            this.pnlTop.TabIndex = 0;
+            // 
+            // pnlBottom
+            // 
+            this.pnlBottom.BackColor = System.Drawing.Color.Navy;
+            this.pnlBottom.Controls.Add(this.lblBottom);
+            this.pnlBottom.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.pnlBottom.Location = new System.Drawing.Point(0, 350);
+            this.pnlBottom.Name = "pnlBottom";
+            this.pnlBottom.Size = new System.Drawing.Size(800, 100);
+            this.pnlBottom.TabIndex = 1;
+            // 
+            // lblTopo
+            // 
+            this.lblTopo.AutoSize = true;
+            this.lblTopo.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblTopo.ForeColor = System.Drawing.Color.White;
+            this.lblTopo.Location = new System.Drawing.Point(151, 34);
+            this.lblTopo.Name = "lblTopo";
+            this.lblTopo.Size = new System.Drawing.Size(495, 31);
+            this.lblTopo.TabIndex = 2;
+            this.lblTopo.Text = "Para testar o array unidimencional-vetor";
+            // 
+            // lblBottom
+            // 
+            this.lblBottom.AutoSize = true;
+            this.lblBottom.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblBottom.ForeColor = System.Drawing.Color.White;
+            this.lblBottom.Location = new System.Drawing.Point(263, 33);
+            this.lblBottom.Name = "lblBottom";
+            this.lblBottom.Size = new System.Drawing.Size(282, 31);
+            this.lblBottom.TabIndex = 0;
+            this.lblBottom.Text = "Clique no botão acima";
+            // 
+            // btnTestar
+            // 
+            this.btnTestar.BackColor = System.Drawing.Color.RoyalBlue;
+            this.btnTestar.Font = new System.Drawing.Font("Microsoft Sans Serif", 72F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnTestar.Location = new System.Drawing.Point(259, 155);
+            this.btnTestar.Name = "btnTestar";
+            this.btnTestar.Size = new System.Drawing.Size(286, 136);
+            this.btnTestar.TabIndex = 2;
+            this.btnTestar.Text = "OK";
+            this.btnTestar.UseVisualStyleBackColor = false;
+            this.btnTestar.Click += new System.EventHandler(this.btnTestar_Click);
+            // 
+            // frmVetor
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.btnTestar);
+            this.Controls.Add(this.pnlBottom);
+            this.Controls.Add(this.pnlTop);
+            this.Name = "frmVetor";
+            this.Text = "vetor";
+            this.pnlTop.ResumeLayout(false);
+            this.pnlTop.PerformLayout();
+            this.pnlBottom.ResumeLayout(false);
+            this.pnlBottom.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel pnlTop;
+        private System.Windows.Forms.Label lblTopo;
+        private System.Windows.Forms.Panel pnlBottom;
+        private System.Windows.Forms.Label lblBottom;
+        private System.Windows.Forms.Button btnTestar;
+    }
+}
+
